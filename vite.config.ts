18592a6c3b5519d61b232b5894d1e93c054421c6
Christa <christatingzon@gmail.com>
2025-07-29 11:48:02 +0800

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,22 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
+  server: {
+    proxy: {
+      '/api/doctors': {
+        target: 'https://v23l6u6a3qrlk6k3sweaql3chm0iaayj.lambda-url.us-west-2.on.aws/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/doctors/, '')
+      }
+    }
+  }
 })
