b02b7ad95941525726328ef985218bc16ec29f68
Christa <christatingzon@gmail.com>
2025-07-29 11:48:29 +0800

    fetch the doctors

--- src/components/DoctorsList.vue
+++ src/components/DoctorsList.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useDoctorStore } from '../stores/doctors'
 import Doctor from './DoctorsListItem.vue'
+
+const doctorStore = useDoctorStore()
+
+onMounted(() => {
+  if (doctorStore.doctors.length === 0) {
+    doctorStore.fetchDoctors().catch((err: Error) => {
+      console.error(err.message)
+    })
+  }
+})
 </script>
 <template>
   <ul class="list bg-base-100 rounded-box shadow-md">
