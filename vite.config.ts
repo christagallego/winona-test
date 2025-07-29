8252e568f72170905db53ae120b9d36a0776b311
Christa <christatingzon@gmail.com>
2025-07-29 14:33:51 +0800

    setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -22,5 +23,8 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api\/doctors/, '')
       }
     }
+  },
+  test: {
+    environment: 'jsdom'
   }
 })
