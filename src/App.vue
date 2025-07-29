54df3e978f5a63ce6d0b012ddb0d7282fad7d2e8
Christa <christatingzon@gmail.com>
2025-07-29 10:29:04 +0800

    add theme switcher to footer

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,30 @@ import ThemeSwitcher from './components/ThemeSwitcher.vue'
 </script>
 
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">Dashboard</a>
+  <div class="min-h-screen flex flex-col">
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">Dashboard</a>
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><a>Doctors</a></li>
+          <li><ThemeSwitcher /></li>
+        </ul>
+      </div>
     </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Doctors</a></li>
-        <li><ThemeSwitcher /></li>
-      </ul>
-    </div>
-  </div>
-  <RouterView />
 
-  <footer>Put a footer with a dark mode toggle here. You can reuse the one from the header</footer>
+    <main class="flex-grow">
+      <RouterView />
+    </main>
+
+    <footer class="bg-base-200 p-4 text-center">
+      <div class="flex justify-between items-center">
+        <span class="text-xs">&copy; Dashboard</span>
+        <ThemeSwitcher />
+      </div>
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>
